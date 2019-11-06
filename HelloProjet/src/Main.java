e29c28631525b60c127b95cb1278c60a2e176cff
caolei <383619529@qq.com>
2019-11-06 15:52:56 +0800

    第十一次提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 
 public class Main {
-    public void testHelloWrold() {
+    public void testHelloWrold() {第十一次修改
 		//第二部分修改
         System.out.println("HelloWorld!");
 		//第三次修改的地方
